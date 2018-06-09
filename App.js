84f997b5ea71a00ae001ad2222cff9943b0ca94d
Danny Williams <danny.h.williams@hotmail.com>
2018-06-09 13:37:16 -0300

    remove stuff

--- App.js
+++ App.js
@@ -1,3 +1,22 @@
-import App from './app/index'
+import React from 'react'
+import { Provider as PaperProvider, Card, CardContent, Title, Paragraph } from 'react-native-paper'
+import { View } from 'react-native'
+
+const Home = () => (
+  <View>
+    <Card>
+      <CardContent>
+        <Title>Card title</Title>
+        <Paragraph>Card content</Paragraph>
+      </CardContent>
+    </Card>
+  </View>
+)
+
+const App = () => (
+  <PaperProvider>
+    <Home />
+  </PaperProvider>
+)
 
 export default App
